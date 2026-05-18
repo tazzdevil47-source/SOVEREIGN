f5f7913f47dde2caa3aa4de7dd17deb7ed55ea2f
tazzdevil47-source <tazzdevil47@gmail.com>
2026-05-18 09:40:47 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE = 'sovereign-v10';
+const ASSETS = [
+  '.',
+  './index.html',
+  './images/intel/91.jpg',
+  './images/intel/76.jpg',
+  './images/intel/61.jpg',
+  './images/intel/41.jpg',
+  './images/intel/26.jpg',
+  './images/intel/11.jpg',
+  './images/intel/0.jpg',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/apple-touch-icon.png',
+  './music/The_Dark_Castle.mp3',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://unpkg.com/prop-types@15.8.1/prop-types.min.js',
+  'https://unpkg.com/recharts@2.12.7/umd/Recharts.js',
+  'https://cdn.sheetjs.com/xlsx-0.20.3/package/dist/xlsx.full.min.js',
+  'https://fonts.googleapis.com/css2?family=Cinzel:wght@400;600;700&family=Crimson+Text:ital,wght@0,400;0,600;1,400&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      Promise.allSettled(ASSETS.map(a => c.add(a).catch(() => {})))
+    ).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (!e.request.url.startsWith('http')) return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res && res.status === 200 && res.type !== 'opaque') {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
